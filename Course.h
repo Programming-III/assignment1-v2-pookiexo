a4ed9b11c91d3aac08ddbb9845487d65c485d90a
pookiexo <aya.kodiera@student.giu-uni.de>
2025-11-11 09:19:49 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -12,6 +12,9 @@ int maxStudents;
 Student* students;
 int currentStudents;
 public:
+Course();
+Course( string courseCode, string courseName, int maxStudents, Student* students, int currentStudents);
+~Course();
 addStudent( const Student& s);
 displayCourseInfo();
 };
